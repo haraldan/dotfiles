d42c4d507a8a7221671cd75ce2c6b503512f8bfb
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-08-12 17:39:54 +0200

    DAP-UI setup

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -28,7 +28,7 @@ return {
       dap.adapters.cppdbg = {
         id = "cppdbg",
         type = "executable",
-        command = "~/.local/share/nvim/mason/bin/OpenDebugAD7",
+        command = vim.fn.expand("~/.local/share/nvim/mason/bin/OpenDebugAD7"),
       }
       dap.configurations.cpp = {
         {
@@ -39,20 +39,13 @@ return {
             return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
           end,
           cwd = "${workspaceFolder}",
-          stopAtEntry = true,
-        },
-        {
-          name = "Attach to gdbserver :1234",
-          type = "cppdbg",
-          request = "launch",
-          MIMode = "gdb",
-          miDebuggerServerAddress = "localhost:1234",
-          miDebuggerPath = "/usr/bin/gdb",
-          cwd = "${workspaceFolder}",
-
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
+          setupCommands = {
+            {
+              text = "-enable-pretty-printing",
+              description = "enable pretty printing",
+              ignoreFailures = false,
+            },
+          },
         },
       }
       dap.configurations.c = dap.configurations.cpp
@@ -72,23 +65,60 @@ return {
         dapui.close()
       end
 
-      vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#d91414'})
-      vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379'})
-      vim.fn.sign_define('DapBreakpoint', {text='●', texthl='DapBreakpoint', linehl='', numhl=''})
-      vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='DapStopped', linehl ='', numhl =''})
+      vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#d91414" })
+      vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379" })
+      vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapStopped", linehl = "", numhl = "" })
 
-      vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
-      vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-      vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-      vim.keymap.set('n', '<F6>', function() require('dap').continue() end)
-      vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-      vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-      vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-      vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-        require('dap.ui.widgets').hover()
-      end)
+      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>dx", dap.terminate, { desc = "DAP: Terminate Session" })
+      vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "DAP: Run Last" })
+      vim.keymap.set("n", "<Leader>dr", dap.restart, { desc = "DAP: Restart Session" })
+      vim.keymap.set("n", "<F6>", dap.continue)
+      vim.keymap.set("n", "<F10>", dap.step_over)
+      vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+        require("dap.ui.widgets").hover()
+      end, { desc = "DAP: Hover" })
+
+      vim.keymap.set("n", "<Leader>dc", dapui.close, { desc = "DAP UI: Close" })
+      vim.keymap.set(
+        { "n", "v" },
+        "<Leader>dw",
+        dapui.elements.watches.add,
+        { desc = "DAP UI: Add to Watch Expressions" }
+      )
+      vim.keymap.set({ "n", "v" }, "<Leader>de", function()
+        dapui.eval()
+        dapui.eval()
+      end, { desc = "DAP UI: Evaluate" })
+
       -- vim.keymap.set("n", "<Leader>dm", require('dap-python').test_method)
       -- vim.keymap.set("v", "<Leader>ds", require('dap-python').debug_selection)
+
+      -- close floats with q or esc
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dap-float",
+        callback = function()
+          vim.api.nvim_buf_set_keymap(0, "n", "q", "<cmd>close!<CR>", { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(0, "n", "<ESC>", "<cmd>close!<CR>", { noremap = true, silent = true })
+        end,
+      })
+
+      -- hover functionality, depending on the environment
+      vim.keymap.set("n", "K", function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        local session = require("dap").session()
+        if not winid then
+          if session ~= nil then
+            require("dapui").eval()
+            require("dapui").eval()
+          else
+            vim.lsp.buf.hover()
+          end
+        end
+      end)
     end,
   },
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,23 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "auto",
-		},
-		sections = {
-			lualine_c = { require("auto-session.lib").current_session_name, "filename" },
-			lualine_x = { "filetype", "%{&fo}" },
-		},
-	},
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+    	options = {
+    		theme = "auto",
+    		ignore_focus = {
+    			"dapui_watches",
+    			"dapui_breakpoints",
+    			"dapui_scopes",
+    			"dapui_console",
+    			"dapui_stacks",
+    			"dap-repl",
+    		},
+    	},
+    	sections = {
+    		lualine_c = { require("auto-session.lib").current_session_name, "filename" },
+    		lualine_x = { "filetype", "%{&fo}" },
+    	},
+    })
+  end,
 }
