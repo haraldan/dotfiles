fe8f92809c0d328a0bbb4e99c7d02d4e813651b5
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-08-19 17:38:36 +0200

    added virt-notes.nvim

--- .config/nvim/lua/plugins/auto-sessions.lua
+++ .config/nvim/lua/plugins/auto-sessions.lua
@@ -5,6 +5,7 @@ return {
 			log_level = "error",
 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
       pre_save_cmds = { "NvimTreeClose" },
+      silent_restore = false,
 			session_lens = {
 				-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
 				buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
