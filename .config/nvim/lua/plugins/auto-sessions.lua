079a4cd4fe903aac3913f0b6ead9e90a889b2e65
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-09-02 14:41:44 +0200

    update auto-sessions config
    update auto-sessions config

--- .config/nvim/lua/plugins/auto-sessions.lua
+++ .config/nvim/lua/plugins/auto-sessions.lua
@@ -1,13 +1,13 @@
 return {
-	"rmagatti/auto-session",
-	config = function()
-		require("auto-session").setup({
-			log_level = "error",
-			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      pre_save_cmds = { "NvimTreeClose" },
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
       silent_restore = false,
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      log_level = "error",
+      pre_save_cmds = { "NvimTreeClose" },
+      lazy_support = true,
 			session_lens = {
-				-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
 				buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
 				load_on_setup = true,
 				theme_conf = { border = true },
