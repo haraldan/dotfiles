dd31230c14127a6dfc94af844a4a3ad1929558d2
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-09-05 14:18:59 +0200

    nvim-dap update

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,5 +1,9 @@
 return {
 	{
+		"mfussenegger/nvim-dap",
+		commit = "f2e96ba3e86d31c6672a715da98717b634c66bd5",
+	},
+	{
 		"jay-babu/mason-nvim-dap.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
@@ -29,6 +33,9 @@ return {
 		},
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
+
+			-- Set up cursor behaviour
+			dap.defaults.fallback.switchbuf = "usevisible,usetab,uselast"
 
 			-- Python setup
 			require("dap-python").setup("python")
@@ -71,43 +78,83 @@ return {
 				dapui.toggle()
 				vim.opt.splitright = true
 			end
+			-- Open Dap UI on DAP session start
 			dap.listeners.before.attach.dapui_config = function()
 				dapui_open()
 			end
 			dap.listeners.before.launch.dapui_config = function()
 				dapui_open()
 			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
+
+			-- Close Dap UI on DAP session termination
+			-- dap.listeners.before.event_terminated.dapui_config = function()
+			-- 	dapui.close()
+			-- end
+			-- dap.listeners.before.event_exited.dapui_config = function()
+			-- 	dapui.close()
+			-- end
+
+			-- Update floating window after debugger advanced
+			local function UpdateHover()
+				for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+					if vim.api.nvim_win_get_config(winid).relative ~= "" then
+						vim.cmd.fclose()
+						require("dapui").eval()
+					end
+				end
 			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
+			dap.listeners.after["event_stopped"]["xxx"] = function()
+				UpdateHover()
 			end
 
-      -- Breakpoint symbols
+			-- Breakpoint symbols
 			vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#d91414" })
+			vim.api.nvim_set_hl(0, "DapBreakpointCondition", { ctermbg = 0, fg = "#2749f2" })
 			vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379" })
 			vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+			vim.fn.sign_define("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
 			vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapStopped", linehl = "", numhl = "" })
 
-      -- Mappings
+			-- Mappings
 			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
-			vim.keymap.set("n", "<Leader>dc", dap.clear_breakpoints, { desc = "DAP: Clear Breakpoints" })
-			vim.keymap.set("n", "<Leader>dx", dap.terminate, { desc = "DAP: Terminate Session" })
+			vim.keymap.set("n", "<Leader>dx", dap.clear_breakpoints, { desc = "DAP: Clear Breakpoints" })
+			vim.keymap.set("n", "<Leader>dq", dap.terminate, { desc = "DAP: Terminate Session" })
 			vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "DAP: Run Last" })
 			vim.keymap.set("n", "<Leader>dr", dap.restart, { desc = "DAP: Restart Session" })
+			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+				require("dap.ui.widgets").hover()
+			end, { desc = "DAP: Hover" })
+			vim.keymap.set("n", "<Leader>dc", function()
+				vim.ui.input({ prompt = "Enter breakpoint condition:" }, function(input)
+					if input ~= "" and input ~= nil then
+						dap.set_breakpoint(input)
+					else
+						vim.notify("Breakpoint condition empty", vim.log.levels.WARN)
+					end
+				end)
+			end, { desc = "DAP: Toggle Conditional Breakpoint" })
+
+			vim.keymap.set("n", "<Leader>dn", function()
+				vim.ui.input({ prompt = "Enter breakpoint hit number:" }, function(input)
+					if input ~= "" and input ~= nil then
+						dap.set_breakpoint("", input)
+					else
+						vim.notify("Breakpoint hit number empty", vim.log.levels.WARN)
+					end
+				end)
+			end, { desc = "DAP: Toggle Hit Number Breakpoint" })
+
 			vim.keymap.set("n", "<F10>", dap.step_over)
 			vim.keymap.set("n", "<F11>", dap.step_into)
 			vim.keymap.set("n", "<F12>", dap.step_out)
 			vim.keymap.set("n", "<F5>", function()
-				if vim.fn.filereadable(".vscode/launch.json") then
-					require("dap.ext.vscode").load_launchjs(".dap/launch.json", { cppdbg = { "c", "cpp" } })
+				if require("dap").status() ~= "Running" then
+					if vim.fn.filereadable(".vscode/launch.json") then
+						require("dap.ext.vscode").load_launchjs(".dap/launch.json", { cppdbg = { "c", "cpp" } })
+					end
+					require("dap").continue()
 				end
-				require("dap").continue()
 			end)
-			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
-				require("dap.ui.widgets").hover()
-			end, { desc = "DAP: Hover" })
 
 			vim.keymap.set("n", "<Leader>du", dapui_toggle, { desc = "DAP UI: Toggle UI" })
 			vim.keymap.set(
@@ -121,7 +168,7 @@ return {
 			-- vim.keymap.set("v", "<Leader>ds", require('dap-python').debug_selection)
 
 			-- hover functionality, depending on the environment
-			vim.keymap.set({"n", "v"}, "K", function()
+			vim.keymap.set({ "n", "v" }, "K", function()
 				local winid = require("ufo").peekFoldedLinesUnderCursor()
 				local session = require("dap").session()
 				if not winid then
@@ -132,8 +179,8 @@ return {
 					end
 				end
 			end)
+			-- Additional mapping for LSP hover
 			vim.keymap.set("n", "<M-K>", vim.lsp.buf.hover)
-
 		end,
 	},
 }
