115ef0d9d5ca7c64f7f1cb27f79e763a5dbdc748
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-09-27 11:40:16 +0200

    Telescope mappings update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -23,8 +23,8 @@ return {
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next,     -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
             ["<C-s>"] = actions.select_horizontal,
             ["<C-\\>"] = actions.select_vertical,
             ["<C-x>"] = false,
@@ -33,7 +33,7 @@ return {
             ["<C-s>"] = actions.select_horizontal,
             ["<C-x>"] = false,
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next,     -- move to next result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-\\>"] = actions.select_vertical,
             ["q"] = actions.close,
           },
@@ -47,10 +47,10 @@ return {
             },
             n = {
               ["<C-x>"] = actions.delete_buffer,
-            }
-          }
-        }
-      }
+            },
+          },
+        },
+      },
     })
 
     require("telescope").load_extension("fzf")
@@ -59,7 +59,7 @@ return {
     require("telescope").load_extension("git_signs")
 
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
-    vim.keymap.set("n", "<leader>fo", builtin.vim_options, { desc = "[F]ind Vim [O]ptions" })
+    vim.keymap.set("n", "<leader>fO", builtin.vim_options, { desc = "[F]ind Vim [O]ptions" })
     vim.keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "[F]ind [J]umps" })
     vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "[F]ind [M]arks" })
     vim.keymap.set("n", "<leader>fG", builtin.git_status, { desc = "[F]ind in [G]it status" })
@@ -73,16 +73,9 @@ return {
     vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
     vim.keymap.set("n", "<leader>f.", builtin.resume, { desc = "Repeat last search" })
-    vim.keymap.set("n", "<leader>fR", builtin.oldfiles, { desc = "[F]ind [R]ecent" })
-    vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "[F]ind [r]egisters" })
+    vim.keymap.set("n", "<leader>fR", builtin.registers, { desc = "[F]ind [R]egisters" })
     vim.keymap.set("n", "<leader>fS", builtin.spell_suggest, { desc = "[F]ind [S]pelling suggestions" })
     vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "[F]ind [Q]uickfix items" })
-
-    vim.keymap.set("n", "<leader>b", function()
-      builtin.buffers({ sort_mru = true })
-    end, { desc = "List open [B]uffers" })
-
-    -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set(
       "n",
       "<leader>/",
@@ -90,19 +83,38 @@ return {
       { desc = "[/] Fuzzily search in current buffer" }
     )
 
-    -- Call functions with options
+    vim.keymap.set("n", "<leader>fr", function ()
+      builtin.oldfiles({promt_title = "Recent Files"})
+    end, 
+      { desc = "[F]ind [r]ecent" })
+    vim.keymap.set("n", "<leader>b", function()
+      builtin.buffers({ sort_mru = true })
+    end, { desc = "List open [B]uffers" })
+
     vim.keymap.set("n", "<leader>fd", function()
       builtin.diagnostics({ bufnr = 0 })
     end, { desc = "[F]ind [D]iagnostics in current buffer" })
 
-    vim.keymap.set("n", "<leader>f/", function()
-      builtin.live_grep({
+    vim.keymap.set("n", "<leader>fo", function()
+      builtin.grep_string({
+        prompt_title = "Fuzzy Search in Open Files",
+        word_match = "-w",
+        only_sort_text = true,
         grep_open_files = true,
-        prompt_title = "Live Grep in Open Files",
+        search = "",
       })
-    end, { desc = "[F]ind [/] in Open Files" })
+    end, { desc = "[F]ind in [O]pen Files" })
 
-    -- Shortcut for searching your Neovim configuration files
+    vim.keymap.set("n", "<leader>fW", function()
+      builtin.grep_string({
+        prompt_title = "Fuzzy Search in Workspace",
+        word_match = "-w",
+        only_sort_text = true,
+        search = "",
+      })
+    end, { desc = "[F]ind in [W]orkspace" })
+
+
     vim.keymap.set("n", "<leader>fn", function()
       builtin.find_files({ cwd = vim.fn.stdpath("config") })
     end, { desc = "[F]ind [N]eovim files" })
