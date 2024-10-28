c738a2fc7dbd43e10f4eaced5c0ab3cb49f3b0ec
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-10-28 10:41:34 +0100

    update auto-sessions config

--- .config/nvim/lua/plugins/auto-sessions.lua
+++ .config/nvim/lua/plugins/auto-sessions.lua
@@ -1,20 +1,20 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    require("auto-session").setup({
-      silent_restore = false,
-      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      log_level = "error",
-      pre_save_cmds = { "NvimTreeClose" },
-      lazy_support = true,
+	"rmagatti/auto-session",
+	config = function()
+		require("auto-session").setup({
+			continue_restore_on_error = false,
+			lazy_support = true,
+			log_level = "error",
+			pre_save_cmds = { "NvimTreeClose" },
 			session_lens = {
 				buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
 				load_on_setup = true,
-				theme_conf = { border = true },
 				previewer = false,
+				theme_conf = { border = true },
 			},
+			suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
 		})
-    vim.o.sessionoptions="buffers,curdir,help,tabpages,folds,winsize,winpos,terminal"
+		vim.o.sessionoptions = "buffers,curdir,help,tabpages,folds,winsize,winpos,terminal,localoptions"
 		vim.keymap.set("n", "<leader>fs", require("auto-session.session-lens").search_session, {
 			noremap = true,
 			desc = "[F]ind [S]essions",
