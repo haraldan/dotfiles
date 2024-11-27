bb687de6f89bc95c6c2b942a0f10f12472a1b597
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-11-27 16:41:16 +0100

    fix DAP console history

--- .tmux/tmux_dap_split.sh
+++ .tmux/tmux_dap_split.sh
@@ -11,9 +11,8 @@ dap_pane_exists="$(tmux has -t $dap_pane_id 2>/dev/null && echo 1)"
 if [[ -z "$dap_pane_exists"  ]]; then
   dap_pane_id=$(tmux split-window -d -l "20%" -P -F "#{pane_id}")
   tmux set -g @dap_pane_id "$dap_pane_id"
+  tmux send-keys -t $dap_pane_id "export PROMPT_COMMAND= ; set +o history" ENTER
 fi
 
-
-
-tmux send-keys -t $dap_pane_id "export PROMPT_COMMAND= ; $command" ENTER
+tmux send-keys -t $dap_pane_id  "$command" ENTER
 
