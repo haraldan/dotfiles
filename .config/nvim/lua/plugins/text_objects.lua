60a40e82df78106daf635d5c0035190a1f0a7563
Daniel Sinev <daniel.sinev@sennheiser.com>
2025-01-17 14:53:29 +0100

    update text_objects config

--- .config/nvim/lua/plugins/text_objects.lua
+++ .config/nvim/lua/plugins/text_objects.lua
@@ -1,25 +1,32 @@
 return {
-	{
-		"wellle/targets.vim",
-    config = function ()
+  {
+    "wellle/targets.vim",
+    config = function()
       -- add {} lua tables to the argument text object
       vim.cmd([[autocmd User targets#mappings#user call targets#mappings#extend({
       \ 'a': {'argument': [{'o': '[([{]', 'c': '[])}]', 's': ','}]},
     \ }) ]])
-    end
-	},
-	{
-		"chrisgrieser/nvim-various-textobjs",
-		lazy = false,
-		config = function()
-			require("various-textobjs").setup({ useDefaultKeymaps = true })
-			-- Remap sentence text objects
-			vim.keymap.set({ "o", "x" }, "aS", "as", {desc = "outer Sentence"})
-			vim.keymap.set({ "o", "x" }, "iS", "is", {desc = "inner Sentence"})
-			-- example: `as` for outer subword, `is` for inner subword
-			vim.keymap.set({ "o", "x" }, "as", '<cmd>lua require("various-textobjs").subword("outer")<CR>', {desc = "outer subword"})
-			vim.keymap.set({ "o", "x" }, "is", '<cmd>lua require("various-textobjs").subword("inner")<CR>', {desc = "inner subword"})
-		end,
-	},
+    end,
+  },
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    lazy = false,
+    config = function()
+      require("various-textobjs").setup({
+        keymaps = {
+          useDefaults = true,
+        },
+      })
+      -- Remap sentence text objects
+      vim.keymap.set({ "o", "x" }, "aS", "as", { desc = "outer Sentence" })
+      vim.keymap.set({ "o", "x" }, "iS", "is", { desc = "inner Sentence" })
+      -- example: `as` for outer subword, `is` for inner subword
+      vim.keymap.set(
+        { "o", "x" }, "as", '<cmd>lua require("various-textobjs").subword("outer")<CR>', { desc = "outer subword" }
+      )
+      vim.keymap.set(
+        { "o", "x" }, "is", '<cmd>lua require("various-textobjs").subword("inner")<CR>', { desc = "inner subword" }
+      )
+    end,
+  },
 }
-
