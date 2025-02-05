f5ae7e7fd88ffafecc0e7c2a27deac1c4731ac53
Daniel Sinev <daniel.sinev@sennheiser.com>
2025-02-05 15:13:17 +0100

    update vim-slime shortcuts

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -30,6 +30,7 @@ return {
 		dependencies = {
 			"linux-cultist/venv-selector.nvim",
 		},
+    ft = "python",
 		init = function()
 			vim.g.slime_no_mappings = 1
 			vim.g.slime_target = "tmux"
@@ -38,8 +39,7 @@ return {
 			vim.cmd([[let g:slime_default_config = {"socket_name": "default", "target_pane": "{right-of}"}]])
 		end,
 		config = function()
-
-			local function update_tmux_pane()
+			local function update_tmux_pane_id()
 				local pane_id = vim.fn.system("~/.tmux/tmux_get_ipython_pane_id.sh")
 				local slime_config = '{"socket_name": "default", "target_pane": "' .. pane_id:sub(1, -2) .. '"}'
 				vim.cmd([[let g:slime_default_config =]] .. slime_config)
@@ -49,31 +49,53 @@ return {
 			local function open_ipython_split()
 				local venv = require("venv-selector").venv()
 				vim.cmd("silent !~/.tmux/tmux_ipython_split.sh " .. venv .. "/bin/ipython --no-autoindent")
-        update_tmux_pane()
+				update_tmux_pane_id()
+			end
+			local function close_ipython_split()
+				vim.cmd("silent !~/.tmux/tmux_ipython_close.sh")
 			end
 
+			vim.keymap.set("n", "<leader>ii", open_ipython_split, { desc = "Open ipython REPL", silent = true })
+			vim.keymap.set( "n", "<leader>iq", close_ipython_split, { desc = "Close Ipython REPL", silent = true })
 
-			vim.keymap.set("n", "<leader>ii", open_ipython_split, { desc = "Open Ipython REPL", silent = true })
+			vim.keymap.set("n", "<leader>ix", function()
+				vim.cmd("SlimeSend1 %reset -f")
+			end, { desc = "Reset ipython REPL", silent = true })
+
+			vim.keymap.set("n", "<leader>ir", function()
+        close_ipython_split()
+        open_ipython_split()
+			end, { desc = "Restart ipython REPL", silent = true })
+
+			vim.keymap.set("n", "<leader>ic", function()
+				vim.cmd("SlimeSend1 clear")
+			end, { desc = "Clear ipython REPL", silent = true })
+
 			vim.keymap.set("n", "<F7>", function()
-        open_ipython_split()
-        vim.cmd("%SlimeSend")
-			end, { desc = "Send buffer to iPython",silent=true})
-			vim.keymap.set("v", "<F7>", function()
-        open_ipython_split()
-				vim.api.nvim_feedkeys( vim.api.nvim_replace_termcodes("<Plug>SlimeRegionSend", true, true, true), "m", true)
-			end, { desc = "Send selection to iPython" })
+				open_ipython_split()
+				vim.cmd("%SlimeSend")
+			end, { desc = "Send buffer to ipython", silent = true })
+
+			vim.keymap.set("n", "<leader>ib", function()
+				open_ipython_split()
+				vim.cmd("%SlimeSend")
+			end, { desc = "Send buffer to ipython", silent = true })
 
 			vim.keymap.set("n", "<leader>il", function()
-        open_ipython_split()
+				open_ipython_split()
 				vim.api.nvim_feedkeys( vim.api.nvim_replace_termcodes("<Plug>SlimeLineSend", true, true, true), "m", true)
-			end, { desc = "Send selection to iPython" })
+			end, { desc = "Send line to ipython" })
 
-			vim.keymap.set(
-				"n",
-				"<leader>iq",
-				":silent !~/.tmux/tmux_ipython_close.sh<CR>",
-				{ desc = "Close Ipython REPL", silent = true }
-			)
+			vim.keymap.set("v", "<F7>", function()
+				open_ipython_split()
+				vim.api.nvim_feedkeys( vim.api.nvim_replace_termcodes("<Plug>SlimeRegionSend", true, true, true), "m", true)
+			end, { desc = "Send selection to ipython" })
+
+			vim.keymap.set("v", "<leader>i", function()
+				open_ipython_split()
+				vim.api.nvim_feedkeys( vim.api.nvim_replace_termcodes("<Plug>SlimeRegionSend", true, true, true), "m", true)
+			end, { desc = "Send selection to ipython" })
+
 		end,
 	},
 	{
