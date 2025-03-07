3628339aac39bc699d78148b10c0fde765e82658
Daniel Sinev <daniel.sinev@sennheiser.com>
2025-03-07 13:26:28 +0100

    fix text objects

--- .config/nvim/lua/plugins/text_objects.lua
+++ .config/nvim/lua/plugins/text_objects.lua
@@ -1,32 +1,28 @@
 return {
-  {
-    "wellle/targets.vim",
-    config = function()
-      -- add {} lua tables to the argument text object
-      vim.cmd([[autocmd User targets#mappings#user call targets#mappings#extend({
-      \ 'a': {'argument': [{'o': '[([{]', 'c': '[])}]', 's': ','}]},
-    \ }) ]])
-    end,
-  },
-  {
-    "chrisgrieser/nvim-various-textobjs",
-    lazy = false,
-    config = function()
-      require("various-textobjs").setup({
-        keymaps = {
-          useDefaults = true,
-        },
-      })
-      -- Remap sentence text objects
-      vim.keymap.set({ "o", "x" }, "aS", "as", { desc = "outer Sentence" })
-      vim.keymap.set({ "o", "x" }, "iS", "is", { desc = "inner Sentence" })
-      -- example: `as` for outer subword, `is` for inner subword
-      vim.keymap.set(
-        { "o", "x" }, "as", '<cmd>lua require("various-textobjs").subword("outer")<CR>', { desc = "outer subword" }
-      )
-      vim.keymap.set(
-        { "o", "x" }, "is", '<cmd>lua require("various-textobjs").subword("inner")<CR>', { desc = "inner subword" }
-      )
-    end,
-  },
+	{
+		"echasnovski/mini.ai",
+		version = false,
+    config = true,
+	},
+	{
+		"chrisgrieser/nvim-various-textobjs",
+		lazy = false,
+		config = function()
+			require("various-textobjs").setup({
+				keymaps = {
+					useDefaults = true,
+          disabledDefaults = {"in","an", "iN","aN"}, -- conflicting with mini.ai "next"
+				},
+			})
+			-- Remap sentence text objects
+			vim.keymap.set({ "o", "x" }, "aS", "as", { desc = "outer Sentence" })
+			vim.keymap.set({ "o", "x" }, "iS", "is", { desc = "inner Sentence" })
+			-- example: `as` for outer subword, `is` for inner subword
+			vim.keymap.set( { "o", "x" }, "as", '<cmd>lua require("various-textobjs").subword("outer")<CR>', { desc = "outer subword" })
+			vim.keymap.set( { "o", "x" }, "is", '<cmd>lua require("various-textobjs").subword("inner")<CR>', { desc = "inner subword" })
+      -- d for "digit"
+			vim.keymap.set( { "o", "x" }, "ad", '<cmd>lua require("various-textobjs").number("outer")<CR>', { desc = "outer number" })
+			vim.keymap.set( { "o", "x" }, "id", '<cmd>lua require("various-textobjs").number("inner")<CR>', { desc = "inner number" })
+		end,
+	},
 }
