f5143f7fafe0572443f7161780776eb884e86ba2
Daniel Sinev <daniel.sinev@gmail.com>
2025-04-13 21:53:10 +0200

    switch vimtex viewer to sioyek

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,12 +1,14 @@
 return {
 	{
 		"lervag/vimtex",
+		lazy = false,
 		init = function()
 			-- Use init for configuration, don't use the more common "config".
-			-- vim.g.vimtex_compiler_latexmk = { aux_dir = "build/" }
-			vim.g.vimtex_view_general_viewer = "open_sumatrapdf.sh"
-			vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+			vim.g.vimtex_compiler_latexmk = { aux_dir = "build/" }
 			vim.g.vimtex_quickfix_open_on_warning = "0"
+			vim.g.vimtex_view_method = "sioyek"
+			vim.g.vimtex_view_sioyek_exe = "sioyek"
+			vim.g.vimtex_callback_progpath = "wsl nvim"
 		end,
 	},
 	{
