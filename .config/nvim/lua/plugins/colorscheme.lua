a9d078a8c9cdae237f4242cfe66106a3412b0efb
Daniel Sinev <daniel.sinev@sennheiser.com>
2025-04-16 12:36:43 +0200

    remove old colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,10 @@
-return{
-  -- {
-  --      "catppuccin/nvim",
-  --      name = "catpuccin",
-  --      lazy = false,
-  --      priority = 1000,
-  -- },
-  -- {
-  -- 	"bluz71/vim-nightfly-colors",
-  -- 	lazy = false,
-  -- 	priority = 1000,
-  -- },
-  {
-      "ellisonleao/gruvbox.nvim",
-  	lazy = false,
-  	priority = 1000,
-  	config = function()
-  --  vim.o.background = "dark" -- or "light" for light mode
-  --  vim.cmd([[colorscheme gruvbox]])
-  end
-
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd[[colorscheme tokyonight-night]]
-    end
-  }
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
 }
-
-
