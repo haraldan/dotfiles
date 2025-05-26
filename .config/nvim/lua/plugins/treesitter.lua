767876116ae923165800e113fd3104b527db8679
Daniel Sinev <daniel.sinev@sennheiser.com>
2025-05-26 16:22:16 +0200

    treesitter: ]f for next function

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,30 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = {"tmux"},
-        },
-        -- indent = { enable = true },
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter", lazy = false, branch = "master", build = ":TSUpdate" },
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = { "tmux" },
+				},
+				-- indent = { enable = true },
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]f"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
