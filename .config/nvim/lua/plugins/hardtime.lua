5dea0c2fcf83d2dab4d353bf7545f5ff27a09ac3
Daniel Sinev <daniel.sinev@sennheiser.com>
2025-06-03 10:33:27 +0200

    hardtime: adjust keys

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -5,5 +5,17 @@ return {
 	dependencies = { "MunifTanjim/nui.nvim" },
 	opts = {
     disable_mouse = false,
+     restricted_keys = {
+      ["h"] = { "n", "x" },
+      ["j"] = { "n", "x" },
+      ["k"] = { "n", "x" },
+      ["l"] = { "n", "x" },
+      ["+"] = {},
+      ["gj"] = {},
+      ["gk"] = {},
+      ["<C-M>"] = {},
+      ["<C-N>"] = {},
+      ["<C-P>"] = {},
+   },
   },
 }
