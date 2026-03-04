0c7ddbb2c2e5a01e2d93de8561786f106d43cc29
Daniel Sinev <daniel.sinev@gmail.com>
2026-03-04 16:36:27 +0100

    treesitter: move to new version, remove treesitter-textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,27 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = { "nvim-treesitter/nvim-treesitter", lazy = false, branch = "master", build = ":TSUpdate" },
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
-					disable = { "tmux" },
-				},
-				-- indent = { enable = true },
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-				textobjects = {
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]f"] = "@function.outer",
-						},
-						goto_previous_start = {
-							["[f"] = "@function.outer",
-						},
-					},
-				},
+			local ts = require("nvim-treesitter")
+			ts.setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+			-- Auto-install parsers and enable treesitter for all filetypes
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local ft = args.match
+					local lang = vim.treesitter.language.get_lang(ft) or ft
+
+					if vim.tbl_contains(ts.get_installed(), lang) then
+						vim.treesitter.start()
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					elseif vim.tbl_contains(ts.get_available(), lang) then
+						ts.install({ lang })
+					end
+				end,
 			})
 		end,
 	},
