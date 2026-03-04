9a21c135fd9096284ed5ff967ccca20e95d98dda
Daniel Sinev <daniel.sinev@gmail.com>
2026-03-04 11:08:41 +0100

    nvim: add ansi.nvim

---
+++ .config/nvim/lua/plugins/ansify.lua
@@ -0,0 +1,10 @@
+return {
+	"0xferrous/ansi.nvim",
+	config = function()
+		require("ansi").setup({
+			auto_enable = false, -- Auto-enable for configured filetypes
+			filetypes = { "log", "ansi" }, -- Filetypes to auto-enable
+			theme = "modern",
+		})
+	end,
+}
