5406477db11c8c4345d3ae867f7978bf5b6a7b05
Daniel Sinev <daniel.sinev@sennheiser.com>
2026-06-08 15:17:59 +0200

    windows: fix PSFzf module

--- windows_install_software.ps1
+++ windows_install_software.ps1
@@ -1,15 +1,13 @@
-# Bootstrap script: run this on a fresh Windows machine before using chezmoi.
-
 winget install twpayne.chezmoi
 winget install gerardog.gsudo
 winget install Microsoft.PowerShell
 winget install psmux
 winget install junegunn.fzf
-Install-Module -Name PSFzf -Scope CurrentUser -Force
 winget install sxyazi.yazi
 winget install Python.Python.3
 winget install JesseDuffield.lazygit
 winget install wez.wezterm
-
 # Yazi optional dependencies (recommended)
 winget install Gyan.FFmpeg 7zip.7zip jqlang.jq oschwartz10612.Poppler sharkdp.fd BurntSushi.ripgrep.MSVC ImageMagick.ImageMagick
+
+pwsh -Command "Install-Module -Name PSFzf -Scope CurrentUser -Force"
