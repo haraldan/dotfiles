de31d936a1b812ad56bbbfba153ffe3de8acb7f6
Daniel Sinev <daniel.sinev@sennheiser.com>
2026-06-08 16:38:08 +0200

    windows: add autohetkey scripts setup

---
+++ dot_autohotkey/KatMouse.ahk
@@ -0,0 +1,14 @@
+﻿#Requires AutoHotkey v2.0+
+#SingleInstance
+TraySetIcon("icons/mouse_white.ico")
+
+MButton::
+{
+    MouseGetPos(&x, &y, &winID)
+
+    if WinExist("ahk_id " winID)
+    {
+        WinMoveBottom("ahk_id " winID)
+    }
+    return
+}
--- windows_install_software.ps1
+++ windows_install_software.ps1
@@ -8,6 +8,7 @@ winget install JesseDuffield.lazygit
 winget install wez.wezterm
 # Yazi optional dependencies (recommended)
 winget install Gyan.FFmpeg 7zip.7zip jqlang.jq oschwartz10612.Poppler sharkdp.fd BurntSushi.ripgrep.MSVC ImageMagick.ImageMagick
+winget install AutoHotkey.AutoHotkey
 winget install psmux
 git clone https://github.com/psmux/psmux-plugins.git "$env:TEMP\psmux-plugins" ; Copy-Item "$env:TEMP\psmux-plugins\ppm" "$env:USERPROFILE\.psmux\plugins\ppm" -Recurse ; Remove-Item "$env:TEMP\psmux-plugins" -Recurse -Force
 
