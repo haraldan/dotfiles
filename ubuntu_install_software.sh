6cc690e0fcdf71655daa789d9973794f04059076
Daniel Sinev <daniel.sinev@sennheiser.com>
2026-06-08 13:27:55 +0200

    .dotfiles: software install scripts

--- ubuntu_install_software.sh
+++ ubuntu_install_software.sh
@@ -6,9 +6,24 @@ set -e
 sudo snap set system experimental.hidden-snap-folder=true
 sudo snap install chezmoi --classic
 
+# Install apt packages
+sudo apt install -y tmux keychain python3-full python3-virtualenvwrapper ripgrep fd-find
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name"' | sed 's/.*"v\([^"]*\)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit lazygit.tar.gz
+
 # Install rustup and cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
+
+# Install bob (Neovim version manager) and Neovim
+cargo install bob-nvim
+bob install stable
+bob use stable
 
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -16,3 +31,9 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Install yazi
 cargo install --force yazi-build
+
+# Install ouch (archive tool used by yazi)
+cargo install ouch
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/HEAD/install.sh | bash
